2adb6022e176c87a6a75d9d02162c24f2aba8ae2
anthony <jwseok0727@gmail.com>
2021-03-13 21:03:02 +0900

    error handling image not found

--- rl2/ext/__init__.py
+++ rl2/ext/__init__.py
@@ -2,12 +2,16 @@ import os
 import requests
 import docker
 
+
 class DockerFlaskClient:
     def __init__(self, image, port=5000):
         self.image = image
-        self.cx = docker.from_env()
+        self.client = docker.from_env()
         # models loading and flask entrypoint are ran in docker entrypoing/command
-        self.container = self.cx.containers.run(image)
+        if self.client.images.get(image):
+            # will raise error and not instantiate if image not exists
+            self.container = self.client.containers.run(image)
+
         self.end_point = 'http://' + str(image) + '/act:' + str(port)
 
     def act(self, obs):
