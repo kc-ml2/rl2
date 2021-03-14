cfe46292206a07980364fe09e7cae54fc0261c2c
anthony <jwseok0727@gmail.com>
2021-03-14 17:47:02 +0900

    docker client: detach and auto_remove docker containers

---
+++ rl2/ext/server.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import docker
+
+
+class DockerFlaskClient:
+    def __init__(self, image, port=5000):
+        self.image = image
+        self.client = docker.from_env()
+        # models loading and flask entrypoint are ran in docker entrypoing/command
+        if self.client.images.get(image):
+            # will raise error and not instantiate if image not exists
+            self.container = self.client.containers.run(image, detach=True, auto_remove=True)
+
+        self.end_point = 'http://' + str(image) + '/act:' + str(port)
+
+    def act(self, obs):
+        action = requests.post(self.end_point, obs)
+
+        return action
